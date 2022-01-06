fbbe0890d5ae368fbca6fc2fb090444764405775
shibuya <65008051+Haruo-es@users.noreply.github.com>
2022-01-06 18:52:42 +0900

    Update shinjuku_map.js

--- shinjuku_map.js
+++ shinjuku_map.js
@@ -120,129 +120,61 @@ function createMarker(i,place) {
     currentInfoWindow = infoWindow;
   });
 }
-
-function initMap() {
-  var latlng = new google.maps.LatLng(35.69092, 139.7002579);
-  var opts = {
-    zoom: 16,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("map_canvas"), opts);
-  
-  createData(shinjuku_data.results);
-  
-  createmarker();
-}
-
-function createmarker(){
-  var cafe_latlng = [];
-  for(var i=0; i<place_cafe.length; i++){
-    cafe_latlng[i] = new google.maps.LatLng(place_cafe[i].lat,place_cafe[i].lng);
-    markers_cafe[i] = new google.maps.Marker({
-      position: cafe_latlng[i], 
-      title: place_cafe[i].name,
-      icon: {
-	url: place_cafe[i].icon_path ,
-	scaledSize: new google.maps.Size( 27, 40 ) ,
-      }
-    });
-    var openingHour = openingHours(fGetWeek(), place_cafe, i)
-    
-    var contentStr = '<a>' + place_cafe[i].name + '<br>●Wi-Fi<br>●新宿駅から' + place_cafe[i].distance + 'm<br>●' + week[fGetWeek()] + '曜日の営業時間：'+ openingHour + '</a>' + '<br><a href=';
-    if(place_cafe[i].website){
-	contentStr = contentStr + place_cafe[i].website + '>ホームページ</a> / '
-    }
-    contentStr = contentStr + '<a href=' + place_cafe[i].url + '>Google検索</a>'
-    
-
-    var infoWindow_cafe = new google.maps.InfoWindow({
-      content: contentStr,
-    });
-
-    google.maps.event.addListener(markers_cafe, 'click', function(){
-      if(currentInfoWindow != null) {
-        currentInfoWindow.close();
-      }
-      infoWindow.open(map, markers_cafe);
-      currentInfoWindow = infoWindow;
-    });
-  }
-  var famires_latlng = [];
-  for(var j=0; j<place_famires.length; j++){
-    famires_latlng[j] = new google.maps.LatLng(place_famires[j].lat,place_famires[j].lng);
-    markers_famires[j] = new google.maps.Marker({
-      position: famires_latlng[j],
-      title: place_famires[j].name,
-      icon: {
-	url: place_famires[j].icon_path ,
-	scaledSize: new google.maps.Size( 27, 40 ) ,
-      }
-    });
-  }
-  var hamburger_latlng = [];
-  for(var k=0; k<place_hamburger.length; k++){
-    hamburger_latlng[k] = new google.maps.LatLng(place_hamburger[k].lat,place_hamburger[k].lng);
-    markers_hamburger[k] = new google.maps.Marker({
-      position: hamburger_latlng[k],
-      title: place_hamburger[k].name,
-      icon: {
-	url: place_humburger[k].icon_path ,
-	scaledSize: new google.maps.Size( 27, 40 ) ,
-      }
-    });
-  }
-  var karaoke_latlng = [];
-  for(var m=0; m<place_karaoke.length; m++){
-    karaoke_latlng[m] = new google.maps.LatLng(place_karaoke[m].lat,place_karaoke[m].lng);
-    markers_karaoke[m] = new google.maps.Marker({
-      position: karaoke_latlng[m],
-      title: place_karaoke[m].name,
-      icon: {
-	url: place_karaoke[m].icon_path ,
-	scaledSize: new google.maps.Size( 27, 40 ) ,
-      }
-    });
-  }
-  var netcafe_latlng = [];
-  for(var n=0; n<place_netcafe.length; n++){
-    netcafe_latlng[n] = new google.maps.LatLng(place_netcafe[n].lat,place_netcafe[n].lng);
-    markers_netcafe[n] = new google.maps.Marker({
-      position: netcafe_latlng[n],
-      title: place_netcafe[n].name,
-      icon: {
-	url: place_netcafe[n].icon_path ,
-	scaledSize: new google.maps.Size( 27, 40 ) ,
-      }
-    });
+function cafe() {
+  for (var i=0; i<place_cafe.length; i++) {
+      createMarker(i, place_cafe);
   }
 }
 
-function cafe(){
+function famires() {
+  for (var i=0; i<place_famires.length; i++) {
+      createMarker(i, place_famires);
+  }
+}
+
+function hamburger() {
+  for (var i=0; i<place_hamburger.length; i++) {
+      createMarker(i, place_hamburger);
+  }
+}
+
+function karaoke() {
+  for (var i=0; i<place_karaoke.length; i++) {
+      createMarker(i, place_karaoke);
+  }
+}
+
+function netcafe() {
+  for (var i=0; i<place_netcafe.length; i++) {
+      createMarker(i, place_netcafe);
+  }
+}
+
+function setcafe(){
   for(var i=0; i<markers_cafe.length; i++){
     markers_cafe[i].setMap(map);
   }
 }
 
-function famires(){
+function setfamires(){
   for(var i=0; i<markers_famires.length; i++){
     markers_famires[i].setMap(map);
   }
 }
 
-function hamburger(){
+function sethamburger(){
   for(var i=0; i<markers_hamburger.length; i++){
     markers_hamburger[i].setMap(map);
   }
 }
 
-function karaoke(){
+function setkaraoke(){
   for(var i=0; i<markers_karaoke.length; i++){
     markers_karaoke[i].setMap(map);
   }
 }
 
-function netcafe(){
+function setnetcafe(){
   for(var i=0; i<markers_netcafe.length; i++){
     markers_netcafe[i].setMap(map);
   }
@@ -266,32 +198,20 @@ function deletemarkersall(){
   }
 }
 
-function cafez(){
-  for(var i=0; i<markers_cafe.length; i++){
-    createMarker(place_cafe);
-  }
-}
-
-function famiresz(){
-  for(var i=0; i<markers_famires.length; i++){
-    createMarker(place_famires);
-  }
-}
-
-function hamburgerz(){
-  for(var i=0; i<markers_hamburger.length; i++){
-    createMarker(place_hamburger);
-  }
-}
-
-function karaokez(){
-  for(var i=0; i<markers_karaoke.length; i++){
-    createMarker(place_karaoke);
-  }
-}
-
-function netcafez(){
-  for(var i=0; i<markers_netcafe.length; i++){
-    createMarker(place_netcafe);
-  }
+function initMap() {
+  console.log("xx3");
+  var target = document.getElementById('map');  
+  var latlng = {lat: 35.7002283, lng: 139.4805254};  //国分寺駅の緯度経度
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: latlng,
+    zoom: 16
+  });
+  createData(kokubunji_data.results);
+  cafe();
+  setcafe(map);
+  famires();
+  hamburger();
+  karaoke();
+  netcafe();
+  console.log("xx4");
 }
